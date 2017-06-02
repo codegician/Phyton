b014d156ae12ddb15374888e0f1168f436133015
Ewart Jackson <ewartjackson@icloud.com>
2017-06-02 10:59:36 -0700

    start of square

--- mindstorm.py
+++ mindstorm.py
@@ -1,5 +1,11 @@
 import turtle
 def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("red")
+    
+    dark = turtle.Turtle()
+    dark.forward(100)
 
+    window.exitonclick()
 
 draw_square()
